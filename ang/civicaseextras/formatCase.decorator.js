c8df43cea6b5078cacead9f85df40defc736fd95
René Olivo <minet.ws@gmail.com>
2018-12-04 23:55:16 +0000

    C51-284: Decorate the format case function so it appends overdue dates data to the case

--- ang/civicaseextras/formatCase.decorator.js
+++ ang/civicaseextras/formatCase.decorator.js
@@ -1,16 +1,26 @@
-(function (angular) {
+(function (angular, caseStatuses) {
   var module = angular.module('civicase');
-
-  console.log('@', angular.module('civicase'));
 
   module.config(function ($provide) {
     $provide.decorator('formatCase', function ($delegate) {
-      var originalFormatCase = $delegate[0];
-      console.log('.', originalFormatCase);
+      var originalFormatCase = $delegate;
 
-      return function formatCaseFactory (formatActivity, ContactsDataService) {
-        console.log('@', formatActivity, ContactsDataService, originalFormatCase);
+      /**
+       * Enhances the formatCase method so it adds information about overfue dates for the case.
+       *
+       * @param {Object} caseItem
+       * @return {Object}
+       */
+      return function formatCase (caseItem) {
+        var isStatusOpen = caseStatuses[caseItem.status_id].grouping === 'Opened';
+        var isModifiedDateOverdue = moment().subtract(3, 'months').isSameOrAfter(caseItem.modified_date);
+        caseItem = originalFormatCase(caseItem);
+        caseItem.overdueDates = {
+          modified_date: isStatusOpen && isModifiedDateOverdue
+        };
+
+        return caseItem;
       };
     });
   });
-})(angular);
+})(angular, CRM.civicase.caseStatuses);
