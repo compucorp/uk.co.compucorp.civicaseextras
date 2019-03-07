2fc4edd3457253f8e6c6af5de8e184fd5cebf6f1
René Olivo <minet.ws@gmail.com>
2019-03-07 23:14:16 +0000

    C51-284: Decorate the format case function so it appends overdue dates information to the case

--- ang/civicaseextras.ang.php
+++ ang/civicaseextras.ang.php
@@ -1,15 +1,20 @@
 <?php
+/**
+ * Get a list of JS files
+ */
+function getCivicaseExtrasJSFiles () {
+  return array_merge(array(
+    'assetBuilder://visual-bundle.js',
+    'ang/civicaseextras.js'
+  ), glob_recursive(dirname(__FILE__) . '/civicaseextras/*.js'));
+}
 
 $settings = [
   'overdueNotificationLimit' => (int) Civi::settings()->get('civicaseOverdueNotificationLimit'),
 ];
 
 return [
-  'js' => [
-    'assetBuilder://visual-bundle.js',
-    'ang/civicaseextras.js',
-    'ang/civicaseextras/*.js',
-  ],
+  'js' => getCivicaseExtrasJSFiles(),
   'css' => [
     'assetBuilder://visual-bundle.css',
     'css/*.css',
